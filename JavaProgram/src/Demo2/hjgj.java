83368d8c902b97d0a20ce55f20d7e2e65a8d157f
Karan <Karan@DESKTOP-L5TSS0V>
2024-08-01 12:00:18 +0530

    hyy

---
+++ JavaProgram/src/Demo2/hjgj.java
@@ -0,0 +1,20 @@
+package Demo2;
+
+public class hjgj {
+
+	public static void main(int i) {
+		System.out.println(55);
+	}
+	public static void main(char a) {
+		System.out.println(50);
+	}
+	public static void main(String[] args) {
+		
+		
+		hjgj.main(10);
+		hjgj.main('b');
+	}
+
+		
+	
+}
